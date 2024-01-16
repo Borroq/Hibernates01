95911a0a30aee4a8f0929a4cf99360b29dd63c5e
Borroq <skrzypek.dsrw@gmail.com>
2024-01-16 23:33:55 +0100

    commit new version. Wykożystanie adnotacji zamiast pliku mapujacego jak wczesniej

--- src/main/java/org/example/BasicDatabaseSave.java
+++ src/main/java/org/example/BasicDatabaseSave.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class BasicDatabaseSave {
     public static void main(String[] args) {
-        System.out.println("xxx");
+
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.ctg.xml").build();
+
+        Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        Employee employee = new Employee();
+        //employee.setId(1); //zmieniono na increment w employee.hbm.xml
+        employee.setName("Damian");
+        employee.setSurname("Skrzypek");
+        employee.setJobTitle("Programmer");
+        employee.setSalary(18000);
+
+        session.save(employee);
+        transaction.commit();
+
+        System.out.println("Employee saved");
+
+        session.close();
+        sessionFactory.close();
+
+
     }
 }
